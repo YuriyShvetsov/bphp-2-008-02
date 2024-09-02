284eb0ba562fb27af97f130f50f50208b75de77c
Yuriy.Shvetsov <irwst@yandex.ru>
2024-09-02 18:00:45 +0300

    add author

--- web/login.php
+++ web/login.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Загрузка файлов в PHP</title>
+    <meta name="author" content="YuriyShvetsov">
+    <title>Авторизация пользователя</title>
 </head>
 <body>
     <form action="post.php" method="post">
